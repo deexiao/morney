3fb869c79f7ac3300aef6f4c5e9d251589e1472a
dixiao <mopjtv@gmail.com>
2021-01-31 14:01:01 +0800

    添加 404

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,10 @@
 import Vue from 'vue'
-import Home from '../views/Home.vue'
+// import Home from '../views/Home.vue'
 import VueRouter from 'vue-router'
 import Money from '@/views/Money.vue'
 import Labels from '@/views/Labels.vue'
 import Statistics from '@/views/Statistics.vue'
+import NotFound from '@/views/NotFound.vue'
 
 Vue.use(VueRouter);
 
@@ -24,7 +25,12 @@ const routes = [
   {
     path: '/statistics',
     component: Statistics
-  }
+  },
+  {
+    // 404
+    path: '*',
+    component: NotFound
+  },
 ];
 
 const router = new VueRouter({
