072db57568913c66cad4e314c7d4cb98944a472c
dixiao <mopjtv@gmail.com>
2021-02-17 14:29:06 +0800

    EditLabel.vue 使用 Vuex

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -3,7 +3,7 @@
     <div class="navBar">
       <Icon class="leftIcon" name="left" @click="goBack" />
       <span class="title">编辑标签</span>
-      <span class="rightIcon"></span>
+      <span class="rightIcon" />
     </div>
     <div class="form-wrapper">
       <FormItem
@@ -24,31 +24,35 @@ import Vue from 'vue'
 import { Component } from 'vue-property-decorator'
 import FormItem from '@/components/Money/FormItem.vue'
 import Button from '@/components/Button.vue'
-import store from '@/store/index2'
-
 @Component({
   components: { Button, FormItem },
 })
 export default class EditLabel extends Vue {
-  tag?: Tag = undefined
+  get tag() {
+    return this.$store.state.currentTag
+  }
   created() {
-    this.tag = store.findTag(this.$route.params.id)
+    const id = this.$route.params.id
+    this.$store.commit('setCurrentTag', id)
     if (!this.tag) {
       this.$router.replace('/404')
     }
   }
   update(name: string) {
     if (this.tag) {
-      store.updateTag(this.tag.id, name)
+      // TODO
+      // store.updateTag(this.tag.id, name);
     }
   }
   remove() {
     if (this.tag) {
-      if (store.removeTag(this.tag.id)) {
-        this.$router.back()
-      } else {
-        window.alert('删除失败')
-      }
+      // TODO
+      return
+      // if (store.removeTag(this.tag.id)) {
+      //   this.$router.back();
+      // } else {
+      //   window.alert('删除失败');
+      // }
     }
   }
   goBack() {
