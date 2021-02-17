b8bfcb33fa9173eaaefa234cf4dd05d668ac9804
dixiao <mopjtv@gmail.com>
2021-02-17 21:51:23 +0800

    update

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,4 +1,4 @@
-<template class="layout-wrapper1">
+<template>
   <div class="layout-wrapper" :class="classPrefix && `${classPrefix}-wrapper`">
     <div class="content" :class="classPrefix && `${classPrefix}-content`">
       <!-- slot: 插槽 -->
@@ -22,18 +22,12 @@ export default {
 .layout-wrapper {
   display: flex;
   flex-direction: column;
-  min-height: 100vh;
-
-  // height: -webkit-fill-available;
-  // height: 100vh;
-  // height: -moz-available; /* WebKit-based browsers will ignore this. */
-  // height: -webkit-fill-available; /* Mozilla-based browsers will ignore this. */
-  // height: fill-available;
+  height: 100vh;
+  // min-height: -webkit-fill-available;
+  min-height: fill-available;
 }
 .content {
   overflow: auto;
   flex-grow: 1;
-  min-height: -webkit-fill-available;
-  min-height: fill-available;
 }
 </style>
